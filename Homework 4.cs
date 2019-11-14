e184fdff649e7dfc9af80ef19331364237040a31
myrick11 <56942167+myrick11@users.noreply.github.com>
2019-11-14 17:21:13 -0500

    Add files via upload

---
+++ Homework 4.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               // question 4 
+               int i;
+               for (i = 1; i < 101; i++)
+                   Console.WriteLine(i);
+
+
+           */
+
+            /*
+            // question 5
+            int temp;
+            temp = 5;
+            if (temp <= 10)
+                Console.WriteLine("Polar Bear");
+            else if (temp <= 20)
+                Console.WriteLine("Penguin");
+            if (temp <= 40)
+                Console.WriteLine("Moose");
+            else if (temp <= 50)
+                Console.WriteLine("Reindeer");
+            if (temp <= 60)
+                Console.WriteLine("Deer");
+            else if (temp <= 70)
+                Console.WriteLine("Turtle");
+            if (temp <= 80)
+                Console.WriteLine("Lion");
+            else if (temp <= 90)
+                Console.WriteLine("Fish");
+            else
+                Console.WriteLine("Default");
+            */
+            /*
+            // question 6
+
+            int caseSwitch = 1;
+            switch (caseSwitch)
+            
+            {  
+                case 1:
+                    Console.WriteLine("Polar Bear");
+                    break;
+                case 2:
+                    Console.WriteLine("Penguin");
+                    break;
+                case 3:
+                    Console.WriteLine("Moose");
+                    break;
+                case 4:
+                    Console.WriteLine("Reindeer");
+                    break;
+                case 5:
+                    Console.WriteLine("Deer");
+                    break;
+                case 6:
+                    Console.WriteLine("Turtle");
+                    break;
+                case 7:
+                    Console.WriteLine("Lion");
+                    break;
+                case 8:
+                    Console.WriteLine("Fish");
+                    break;
+                case 9:
+                    Console.WriteLine("Bug");
+                    break;
+
+                */
+            // question 7
+            int i;
+            for (i = 10; i <21; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            }
+                
+            
+           
+             
+      
+        }
+
+      
+
+
+   }
+
+
+
+
+
